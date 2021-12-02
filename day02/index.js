a58b4de8c8f2a80fd855be181a642a29700b54c3
hildasandberg <31202022+hildasandberg@users.noreply.github.com>
2021-12-02 13:38:23 +0100

    Day2 soulution wohoo

--- day02/index.js
+++ day02/index.js
@@ -1,14 +1,35 @@
-function getSolutionPart1() {
-    return 1337;
+const fs = require("fs")
+const inputDataLines = (filename = "input.txt") => {
+  return fs.readFileSync(filename).toString().trim().split("\n")
 }
-
-function getSolutionPart2() {
-    return 42;
+const getSolutionPart1 = () => {
+  const position = { h: 0, d: 0 }
+  inputDataLines().map((e) => {
+    if (e.trim().split(" ")[0] == "down") {
+      position.d = position.d + parseInt(e.trim().split(" ")[1])
+    } else if (e.trim().split(" ")[0] == "up") {
+      position.d = position.d - parseInt(e.trim().split(" ")[1])
+    } else position.h = position.h + parseInt(e.trim().split(" ")[1])
+  })
+  return position.h * position.d
 }
-
-const part = process.env.part || "part1";
-
-if (part === "part1")
-    console.log(getSolutionPart1());
-else
-    console.log(getSolutionPart2());
+const getSolutionPart2 = () => {
+  const pos = { h: 0, d: 0, a: 0 }
+  inputDataLines().map((element) => {
+    if (element.trim().split(" ")[0] == "forward") {
+      pos.h = pos.h + parseInt(element.trim().split(" ")[1])
+      pos.d = pos.d + pos.a * parseInt(element.trim().split(" ")[1])
+    } else if (element.trim().split(" ")[0] == "down") {
+      pos.a = pos.a + parseInt(element.trim().split(" ")[1])
+    } else pos.a = pos.a - parseInt(element.trim().split(" ")[1])
+  })
+  return pos.h * pos.d
+}
+console.log("Javascript")
+const part = process.env.part || "part1"
+if (part === "part1") console.log(getSolutionPart1())
+else console.log(getSolutionPart2())
+module.exports = {
+  getSolutionPart1,
+  getSolutionPart2,
+}
---
+++ day02/test/index.test.js
@@ -0,0 +1,9 @@
+const { getSolutionPart1, getSolutionPart2 } = require("../index")
+
+test("part1", () => {
+  expect(getSolutionPart1()).toBe(2215080)
+})
+
+test("part2", () => {
+  expect(getSolutionPart2()).toBe(1864715580)
+})
